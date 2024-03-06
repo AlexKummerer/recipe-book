47651075670cf73794de84ffb8175e9f62e35c32
Kummey <alexander@thomas-kummerer.de>
2024-03-06 23:53:45 +0100

    create recipe list and item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss'
+  styleUrl: './recipe-list.component.scss',
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      uuid.v4(),
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_960_720.jpg'
+    ),
 
+    new Recipe(
+      uuid.v4(),
+      'Second Test Recipe',
+      'This is second simply a test',
+      'https://cdn.pixabay.com/photo/2017/06/02/18/24/watermelon-2367029_960_720.jpg'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit() {
+    console.log(this.recipes);
+
+
+
+  }
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,15 @@
+export class Recipe {
+  public id: string;
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(id: string, name: string, desc: string, imagePath: string) {
+    this.id = id;
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+
+
+}
