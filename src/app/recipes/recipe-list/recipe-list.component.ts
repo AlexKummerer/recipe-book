500fedb2883a404b934ae047ea177d59fdad5990
Kummey <alexander@thomas-kummerer.de>
2024-03-10 17:07:55 +0100

    image preview

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,29 +1,38 @@
 import { RecipeService } from './../recipe.service';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import * as uuid from 'uuid';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.scss',
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
-
+  subscription: Subscription;
   selectedRecipe: Recipe;
 
-  constructor(private RecipeService :RecipeService  ) {}
+  constructor(private RecipeService: RecipeService) {}
 
   ngOnInit() {
-    console.log('RecipeListComponent.ngOnInit')
+    console.log('RecipeListComponent.ngOnInit');
     this.recipes = this.RecipeService.getRecipes();
-    this.RecipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
-      this.recipes = recipes;
-    });
-
-
+    this.subscription = this.RecipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
   }
 
-
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
